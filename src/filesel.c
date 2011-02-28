warvair/grafx2

commit 1a3c5d2eb9c12519e256625e7b58419b1dc97530

Stable branch (2.2): Atari Falcon port, by Saulot. Includes several big-endian and padding fixes. Backported from 2.3 branch the support for pnglib 1.4
git-svn-id: svn://pulkomandy.tk/GrafX2/branches/release@1709 416bcca6-2ee7-4201-b75f-2eb2f807beb1

src/filesel.c
@@ -2,6 +2,7 @@
 */
 /*  Grafx2 - The Ultimate 256-color bitmap paint program
 
+    Copyright 2011 Pawel Góralski
     Copyright 2009 Franck Charlet
     Copyright 2008 Peter Gordon
     Copyright 2008 Yves Rizoud
@@ -33,6 +34,11 @@
 
 #if defined(__amigaos4__) || defined(__AROS__) || defined(__MORPHOS__) || defined(__amigaos__)
     #include <proto/dos.h>
+    #include <dirent.h>
+    #define isHidden(x) (0)
+
+#elif defined (__MINT__)
+    #include <mint/sysbind.h>
     #include <dirent.h>
     #define isHidden(x) (0)
 #elif defined(__WIN32__)
@@ -273,8 +279,25 @@ void Read_list_of_files(T_Fileselector *list, byte selected_format)
   // Après effacement, il ne reste ni fichier ni répertoire dans la liste
 
   // On lit tous les répertoires:
+
+#if defined (__MINT__)
+  static char path[1024];
+  static char path2[1024];
+  path[0]='\0';
+  path2[0]='\0';
+  
+  char currentDrive='A';
+  currentDrive=currentDrive+Dgetdrv(); 
+  
+  Dgetpath(path,0);
+  sprintf(path2,"%c:\%s",currentDrive,path);
+ 
+  strcat(path2,PATH_SEPARATOR);
+  current_directory=opendir(path2);
+#else  
   current_path=getcwd(NULL,0);
   current_directory=opendir(current_path);
+#endif
   while ((entry=readdir(current_directory)))
   {
     // On ignore le répertoire courant
@@ -323,10 +346,33 @@ void Read_list_of_files(T_Fileselector *list, byte selected_format)
 #if defined(__MORPHOS__) || defined(__AROS__) || defined (__amigaos4__) || defined(__amigaos__)
   Add_element_to_list(list, "/",1); // on amiga systems, / means parent. And there is no ..
   list->Nb_directories ++;
+#elif defined (__MINT__)
+  T_Fileselector_item *item=NULL;
+  // check if ".." exists if not add it
+  // FreeMinT lists ".." already, but this is not so for TOS 
+  // simply adding it will cause double PARENT_DIR under FreeMiNT
+  
+  bool bFound= false;
+  
+  for (item = list->First; (((item != NULL) && (bFound==false))); item = item->Next){
+    if (item->Type == 1){
+	if(strncmp(item->Full_name,"..",(sizeof(char)*2))==0) bFound=true;
+    }
+  }
+  
+  if(!bFound){
+    Add_element_to_list(list, "..",1); // add if not present
+    list->Nb_directories ++;  
+  }
+  
 #endif
 
   closedir(current_directory);
+#if defined (__MINT__)  
+
+#else
   free(current_path);
+#endif
   current_path = NULL;
 
   Recount_files(list);
@@ -422,6 +468,23 @@ void Read_list_of_drives(T_Fileselector *list)
       }
     }
   }
+  #elif defined(__MINT__)
+    char drive_name[]="A:\\";
+    unsigned long drive_bits = Drvmap(); //get drive map bitfield
+    int drive_index;
+    int bit_index;
+    drive_index = 0;
+    for (bit_index=0; bit_index<32; bit_index++)
+    {
+      if ( (1 << bit_index) & drive_bits )
+      {
+        drive_name[0]='A'+bit_index;
+        Add_element_to_list(list, drive_name,2);
+        list->Nb_directories++;
+        drive_index++;
+      }
+    }
+  
   #else
   {
     //Sous les différents unix, on va mettre
@@ -1159,13 +1222,30 @@ byte Button_Load_or_Save(byte load, T_IO_Context *context)
   // On prend bien soin de passer dans le répertoire courant (le bon qui faut! Oui madame!)
   if (load)
   {
+  #if defined(__MINT__)    
+    chdir(Main_current_directory);
+    static char path[1024]={0};
+    Dgetpath(path,0);
+    strcat(path,PATH_SEPARATOR);
+    strcpy(Main_current_directory,path);  
+  #else
     chdir(Main_current_directory);
     getcwd(Main_current_directory,256);
+  #endif
   }
   else
   {
+    #if defined(__MINT__)    
     chdir(context->File_directory);
+    static char path[1024]={0};
+    Dgetpath(path,0);
+    strcat(path,PATH_SEPARATOR);
+    strcpy(Main_current_directory,path);  
+ #else
     getcwd(Main_current_directory,256);
+  #endif
+
+
   }
   
   // Affichage des premiers fichiers visibles:
@@ -1641,8 +1721,15 @@ byte Button_Load_or_Save(byte load, T_IO_Context *context)
         // On doit rentrer dans le répertoire:
         if (!chdir(Selector_filename))
         {
+        #if defined (__MINT__)          
+          static char path[1024]={0};
+          char currentDrive='A';
+          currentDrive=currentDrive+Dgetdrv();
+          Dgetpath(path,0);
+         sprintf(Main_current_directory,"%c:\%s",currentDrive,path);
+        #else
           getcwd(Main_current_directory,256);
-  
+        #endif
           // On lit le nouveau répertoire
           Read_list_of_files(&Filelist, Main_format);
           Sort_list_of_files(&Filelist);

src/Makefile.dep
@@ -1,3 +1,4 @@
+$(OBJDIR)/SFont.o: SFont.c SFont.h
 $(OBJDIR)/brush.o: brush.c global.h struct.h const.h graph.h misc.h errors.h \
  windows.h sdlscreen.h brush.h
 $(OBJDIR)/brush_ops.o: brush_ops.c brush.h struct.h const.h buttons.h engine.h \
@@ -5,7 +6,7 @@ $(OBJDIR)/brush_ops.o: brush_ops.c brush.h struct.h const.h buttons.h engine.h \
 $(OBJDIR)/buttons.o: buttons.c const.h struct.h global.h misc.h graph.h engine.h \
  readline.h filesel.h loadsave.h init.h buttons.h operatio.h pages.h \
  palette.h errors.h readini.h saveini.h shade.h io.h help.h text.h \
- sdlscreen.h windows.h brush.h input.h special.h
+ sdlscreen.h windows.h brush.h input.h special.h setup.h
 $(OBJDIR)/buttons_effects.o: buttons_effects.c buttons.h struct.h const.h engine.h \
  global.h graph.h help.h input.h misc.h readline.h sdlscreen.h windows.h
 $(OBJDIR)/engine.o: engine.c const.h struct.h global.h graph.h misc.h special.h \
@@ -39,7 +40,7 @@ $(OBJDIR)/layers.o: layers.c const.h struct.h global.h windows.h engine.h pages.
 $(OBJDIR)/libraw2crtc.o: libraw2crtc.c const.h global.h struct.h loadsave.h
 $(OBJDIR)/loadsave.o: loadsave.c buttons.h struct.h const.h errors.h global.h io.h \
  loadsave.h misc.h graph.h op_c.h pages.h palette.h sdlscreen.h windows.h \
- engine.h
+ engine.h setup.h
 $(OBJDIR)/main.o: main.c const.h struct.h global.h graph.h misc.h init.h buttons.h \
  engine.h pages.h loadsave.h sdlscreen.h errors.h readini.h saveini.h \
  io.h text.h setup.h windows.h brush.h palette.h realpath.h
@@ -47,7 +48,7 @@ $(OBJDIR)/misc.o: misc.c struct.h const.h sdlscreen.h global.h errors.h buttons.
  engine.h misc.h keyboard.h windows.h palette.h input.h graph.h pages.h
 $(OBJDIR)/miscfileformats.o: miscfileformats.c engine.h struct.h const.h errors.h \
  global.h io.h libraw2crtc.h loadsave.h misc.h sdlscreen.h windows.h
-$(OBJDIR)/mountlist.o: mountlist.c mountlist.h
+$(OBJDIR)/mountlist.o: mountlist.c
 $(OBJDIR)/op_c.o: op_c.c op_c.h struct.h const.h errors.h
 $(OBJDIR)/operatio.o: operatio.c const.h struct.h global.h misc.h engine.h graph.h \
  operatio.h buttons.h pages.h errors.h sdlscreen.h brush.h windows.h
@@ -63,32 +64,32 @@ $(OBJDIR)/pxquad.o: pxquad.c global.h struct.h const.h sdlscreen.h misc.h graph.
  pxquad.h
 $(OBJDIR)/pxsimple.o: pxsimple.c global.h struct.h const.h sdlscreen.h misc.h \
  graph.h pxsimple.h
-$(OBJDIR)/pxtall2.o: pxtall2.c global.h struct.h const.h sdlscreen.h misc.h graph.h \
- pxtall2.h
 $(OBJDIR)/pxtall.o: pxtall.c global.h struct.h const.h sdlscreen.h misc.h graph.h \
  pxtall.h pxsimple.h
+$(OBJDIR)/pxtall2.o: pxtall2.c global.h struct.h const.h sdlscreen.h misc.h graph.h \
+ pxtall2.h
 $(OBJDIR)/pxtriple.o: pxtriple.c global.h struct.h const.h sdlscreen.h misc.h \
  graph.h pxtriple.h
-$(OBJDIR)/pxwide2.o: pxwide2.c global.h struct.h const.h sdlscreen.h misc.h graph.h \
- pxwide2.h
 $(OBJDIR)/pxwide.o: pxwide.c global.h struct.h const.h sdlscreen.h misc.h graph.h \
  pxwide.h
-$(OBJDIR)/readini.o: readini.c const.h errors.h global.h struct.h misc.h readini.h
+$(OBJDIR)/pxwide2.o: pxwide2.c global.h struct.h const.h sdlscreen.h misc.h graph.h \
+ pxwide2.h
+$(OBJDIR)/readini.o: readini.c const.h errors.h global.h struct.h misc.h readini.h \
+ setup.h
 $(OBJDIR)/readline.o: readline.c const.h struct.h global.h misc.h errors.h \
  sdlscreen.h readline.h windows.h input.h
 $(OBJDIR)/realpath.o: realpath.c
 $(OBJDIR)/saveini.o: saveini.c const.h global.h struct.h readini.h io.h errors.h \
- misc.h saveini.h
+ misc.h saveini.h setup.h
 $(OBJDIR)/sdlscreen.o: sdlscreen.c global.h struct.h const.h sdlscreen.h errors.h \
  misc.h
 $(OBJDIR)/setup.o: setup.c struct.h const.h io.h setup.h
-$(OBJDIR)/SFont.o: SFont.c SFont.h
 $(OBJDIR)/shade.o: shade.c global.h struct.h const.h graph.h engine.h errors.h \
  misc.h readline.h help.h sdlscreen.h windows.h input.h shade.h
 $(OBJDIR)/special.o: special.c const.h struct.h global.h graph.h engine.h windows.h \
  special.h pages.h misc.h buttons.h
 $(OBJDIR)/text.o: text.c SFont.h struct.h const.h global.h sdlscreen.h io.h \
- errors.h
+ errors.h setup.h
 $(OBJDIR)/transform.o: transform.c global.h struct.h const.h transform.h engine.h \
  sdlscreen.h windows.h input.h help.h misc.h readline.h buttons.h pages.h
 $(OBJDIR)/version.o: version.c

src/buttons.c
@@ -2,6 +2,7 @@
 */
 /*  Grafx2 - The Ultimate 256-color bitmap paint program
 
+    Copyright 2011 Pawel Góralski
     Copyright 2008 Yves Rizoud
     Copyright 2007-2010 Adrien Destugues (PulkoMandy)
     Copyright 1996-2001 Sunset Design (Guillaume Dorme & Karl Maritaud)
@@ -57,6 +58,7 @@
 #include "brush.h"
 #include "input.h"
 #include "special.h"
+#include "setup.h"
 
 #ifdef __VBCC__
     #define __attribute__(x)
@@ -64,6 +66,10 @@
 
 #if defined(__amigaos4__) || defined(__AROS__) || defined(__MORPHOS__) || defined(__amigaos__)
     #include <proto/dos.h>
+    #include <dirent.h>
+    #define isHidden(x) (0)
+#elif defined(__MINT__)
+    #include <mint/sysbind.h>
     #include <dirent.h>
     #define isHidden(x) (0)
 #elif defined(__WIN32__)
@@ -920,7 +926,7 @@ char * Format_font_filename(const char * fname)
   int c;
   int length;
   
-  fname+=5; // Assume "font_" prefix
+  fname+=strlen(FONT_PREFIX); // Omit file prefix
   length=strlen(fname) - 4; // assume .png extension
   
   for (c=0;c<11 && c<length ;c++)
@@ -947,7 +953,7 @@ void Add_font_or_skin(const char *name)
   else
     fname = name;
   namelength = strlen(fname);
-  if (namelength>=10 && fname[0]!='_' && !strncasecmp(fname, "skin_", 5)
+  if (namelength>=10 && fname[0]!='_' && !strncasecmp(fname, SKIN_PREFIX, strlen(SKIN_PREFIX))
     && (!strcasecmp(fname + namelength - 4,".png")
     || !strcasecmp(fname + namelength - 4,".gif")))
   {
@@ -963,7 +969,7 @@ void Add_font_or_skin(const char *name)
       Format_filename(Skin_files_list.First->Full_name, 0)
     );
   }
-  else if (namelength>=10 && !strncasecmp(fname, "font_", 5)
+  else if (namelength>=10 && !strncasecmp(fname, FONT_PREFIX, strlen(FONT_PREFIX))
     && (!strcasecmp(fname + namelength - 4, ".png")))
   {
     Add_element_to_list(&Font_files_list, name, 0);
@@ -1028,7 +1034,7 @@ void Button_Skins(void)
   Free_fileselector_list(&Font_files_list);
   // Browse the "skins" directory
   strcpy(skinsdir, Data_directory);
-  strcat(skinsdir, "skins");
+  strcat(skinsdir, SKINS_SUBDIRECTORY);
   // Add each found file to the list
   For_each_file(skinsdir, Add_font_or_skin);
   // Sort it
@@ -1235,9 +1241,11 @@ void Button_Page(void)
   Exchange_main_and_spare();
 
   // On fait le reste du travail "à la main":
+#ifndef NOLAYERS
   SWAP_PBYTES(Main_visible_image.Image,Spare_visible_image.Image)
   SWAP_WORDS (Main_visible_image.Width,Spare_visible_image.Width)
   SWAP_WORDS (Main_visible_image.Height,Spare_visible_image.Height)
+#endif 
   SWAP_SHORTS(Main_offset_X,Spare_offset_X)
   SWAP_SHORTS(Main_offset_Y,Spare_offset_Y)
   SWAP_SHORTS(Old_main_offset_X,Old_spare_offset_X)

src/fileformats.c
@@ -2,6 +2,7 @@
 */
 /*  Grafx2 - The Ultimate 256-color bitmap paint program
 
+    Copyright 2011 Pawel Góralski
     Copyright 2009 Petter Lindquist
     Copyright 2008 Yves Rizoud
     Copyright 2008 Franck Charlet
@@ -59,11 +60,11 @@ void Test_IMG(T_IO_Context * context)
   if ((file=fopen(filename, "rb")))
   {
     // Lecture et vérification de la signature
-    if (Read_bytes(file,IMG_header.Filler1,sizeof(IMG_header.Filler1))
+    if (Read_bytes(file,IMG_header.Filler1,6)
     && Read_word_le(file,&(IMG_header.Width))
     && Read_word_le(file,&(IMG_header.Height))
-    && Read_bytes(file,IMG_header.Filler2,sizeof(IMG_header.Filler2))
-    && Read_bytes(file,IMG_header.Palette,sizeof(IMG_header.Palette))
+    && Read_bytes(file,IMG_header.Filler2,118)
+    && Read_bytes(file,IMG_header.Palette,sizeof(T_Palette))
     )
     {
       if ( (!memcmp(IMG_header.Filler1,signature,6))
@@ -94,11 +95,11 @@ void Load_IMG(T_IO_Context * context)
   {
     file_size=File_length_file(file);
 
-    if (Read_bytes(file,IMG_header.Filler1,sizeof(IMG_header.Filler1))
+    if (Read_bytes(file,IMG_header.Filler1,6)
     && Read_word_le(file,&(IMG_header.Width))
     && Read_word_le(file,&(IMG_header.Height))
-    && Read_bytes(file,IMG_header.Filler2,sizeof(IMG_header.Filler2))
-    && Read_bytes(file,IMG_header.Palette,sizeof(IMG_header.Palette))
+    && Read_bytes(file,IMG_header.Filler2,118)
+    && Read_bytes(file,IMG_header.Palette,sizeof(T_Palette))
     )
     {
 
@@ -167,11 +168,11 @@ void Save_IMG(T_IO_Context * context)
 
     memcpy(IMG_header.Palette,context->Palette,sizeof(T_Palette));
 
-    if (Write_bytes(file,IMG_header.Filler1,sizeof(IMG_header.Filler1))
+    if (Write_bytes(file,IMG_header.Filler1,6)
     && Write_word_le(file,IMG_header.Width)
     && Write_word_le(file,IMG_header.Height)
-    && Write_bytes(file,IMG_header.Filler2,sizeof(IMG_header.Filler2))
-    && Write_bytes(file,IMG_header.Palette,sizeof(IMG_header.Palette))
+    && Write_bytes(file,IMG_header.Filler2,118)
+    && Write_bytes(file,IMG_header.Palette,sizeof(T_Palette))
     )
 
     {
@@ -204,7 +205,6 @@ void Save_IMG(T_IO_Context * context)
 
 
 //////////////////////////////////// LBM ////////////////////////////////////
-#pragma pack(1)
 typedef struct
 {
   word  Width;
@@ -221,7 +221,6 @@ typedef struct
   word  X_screen;
   word  Y_screen;
 } T_LBM_Header;
-#pragma pack()
 
 byte * LBM_buffer;
 FILE *LBM_file;
@@ -1554,7 +1553,6 @@ void Save_BMP(T_IO_Context * context)
 
 
 //////////////////////////////////// GIF ////////////////////////////////////
-#pragma pack(1)
 typedef struct
 {
   word Width;   // Width of the complete image area
@@ -1573,7 +1571,6 @@ typedef struct
   byte Indicator;     // Misc image information
   byte Nb_bits_pixel; // Nb de bits par pixel
 } T_GIF_IDB;          // Image Descriptor Block
-#pragma pack()
 
 typedef struct
 {
@@ -2191,9 +2188,16 @@ void Save_GIF(T_IO_Context * context)
           Write_byte(GIF_file,LSDB.Aspect) )
       {
         // Le LSDB a été correctement écrit.
-
+        int i;
         // On sauve la palette
-        if (Write_bytes(GIF_file,context->Palette,768))
+        for(i=0;i<256 && !File_error;i++)
+        {
+          if (!Write_byte(GIF_file,context->Palette[i].R)
+            ||!Write_byte(GIF_file,context->Palette[i].G)
+            ||!Write_byte(GIF_file,context->Palette[i].B))
+            File_error=1;
+        }
+        if (!File_error)
         {
           // La palette a été correctement écrite.
 
@@ -2457,7 +2461,6 @@ void Save_GIF(T_IO_Context * context)
 
 
 //////////////////////////////////// PCX ////////////////////////////////////
-#pragma pack(1)
 typedef struct
   {
     byte Manufacturer;       // |_ Il font chier ces cons! Ils auraient pu
@@ -2479,7 +2482,6 @@ typedef struct
     word Screen_Y;           // |  l'écran d'origine
     byte Filler[54];         // Ca... J'adore!
   } T_PCX_Header;
-#pragma pack()
 
 T_PCX_Header PCX_header;
 
@@ -2925,14 +2927,14 @@ void Save_PCX(T_IO_Context * context)
         Write_word_le(file,PCX_header.Y_max) &&
         Write_word_le(file,PCX_header.X_dpi) &&
         Write_word_le(file,PCX_header.Y_dpi) &&
-        Write_bytes(file,&(PCX_header.Palette_16c),sizeof(PCX_header.Palette_16c)) &&        
+        Write_bytes(file,&(PCX_header.Palette_16c),48) &&
         Write_bytes(file,&(PCX_header.Reserved),1) &&
         Write_bytes(file,&(PCX_header.Plane),1) &&
         Write_word_le(file,PCX_header.Bytes_per_plane_line) &&
         Write_word_le(file,PCX_header.Palette_info) &&
         Write_word_le(file,PCX_header.Screen_X) &&
         Write_word_le(file,PCX_header.Screen_Y) &&
-        Write_bytes(file,&(PCX_header.Filler),sizeof(PCX_header.Filler)) )
+        Write_bytes(file,&(PCX_header.Filler),54) )
     {
       line_size=PCX_header.Bytes_per_plane_line*PCX_header.Plane;
      
@@ -3017,7 +3019,7 @@ void Test_SCx(T_IO_Context * context)
   if ((file=fopen(filename, "rb")))
   {
     // Lecture et vérification de la signature
-    if (Read_bytes(file,SCx_header.Filler1,sizeof(SCx_header.Filler1))
+    if (Read_bytes(file,SCx_header.Filler1,4)
     && Read_word_le(file, &(SCx_header.Width))
     && Read_word_le(file, &(SCx_header.Height))
     && Read_byte(file, &(SCx_header.Filler2))
@@ -3053,7 +3055,7 @@ void Load_SCx(T_IO_Context * context)
   {
     file_size=File_length_file(file);
 
-    if (Read_bytes(file,SCx_header.Filler1,sizeof(SCx_header.Filler1))
+    if (Read_bytes(file,SCx_header.Filler1,4)
     && Read_word_le(file, &(SCx_header.Width))
     && Read_word_le(file, &(SCx_header.Height))
     && Read_byte(file, &(SCx_header.Filler2))
@@ -3174,7 +3176,7 @@ void Save_SCx(T_IO_Context * context)
     SCx_header.Filler2=0xAF;
     SCx_header.Planes=0x00;
 
-    if (Write_bytes(file,SCx_header.Filler1,sizeof(SCx_header.Filler1))
+    if (Write_bytes(file,SCx_header.Filler1,4)
     && Write_word_le(file, SCx_header.Width)
     && Write_word_le(file, SCx_header.Height)
     && Write_byte(file, SCx_header.Filler2)
@@ -3286,8 +3288,8 @@ void Load_PNG(T_IO_Context * context)
             
               // Load file information
               png_read_info(png_ptr, info_ptr);
-              color_type = info_ptr->color_type;
-              bit_depth = info_ptr->bit_depth;
+              color_type = png_get_color_type(png_ptr,info_ptr);
+              bit_depth = png_get_bit_depth(png_ptr,info_ptr);
               
               // If it's any supported file
               // (Note: As of writing this, this test covers every possible 
@@ -3340,9 +3342,9 @@ void Load_PNG(T_IO_Context * context)
                   }
                 }
                 if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_RGB_ALPHA)
-                  Pre_load(context,info_ptr->width,info_ptr->height,File_length_file(file),FORMAT_PNG,PIXEL_SIMPLE,1);
+                  Pre_load(context,png_get_image_width(png_ptr,info_ptr),png_get_image_height(png_ptr,info_ptr),File_length_file(file),FORMAT_PNG,PIXEL_SIMPLE,1);
                 else
-                  Pre_load(context, info_ptr->width,info_ptr->height,File_length_file(file),FORMAT_PNG,context->Ratio,0);
+                  Pre_load(context,png_get_image_width(png_ptr,info_ptr),png_get_image_height(png_ptr,info_ptr),File_length_file(file),FORMAT_PNG,context->Ratio,0);
 
                 if (File_error==0)
                 {
@@ -3421,6 +3423,8 @@ void Load_PNG(T_IO_Context * context)
                   // Transparency (tRNS)
                   if (png_get_tRNS(png_ptr, info_ptr, &trans, &num_trans, &trans_values))
                   {
+                    if (color_type == PNG_COLOR_TYPE_PALETTE && trans!=NULL)
+                    {
                     int i;
                     for (i=0; i<num_trans; i++)
                     {
@@ -3432,9 +3436,24 @@ void Load_PNG(T_IO_Context * context)
                       }
                     }
                   }
+                    else if ((color_type == PNG_COLOR_TYPE_GRAY
+                      || color_type == PNG_COLOR_TYPE_RGB) && trans_values!=NULL)
+                    {
+                      // In this case, num_trans is supposed to be "1", 
+                      // and trans_values[0] contains the reference color
+                      // (RGB triplet) that counts as transparent.
                   
-                  context->Width=info_ptr->width;
-                  context->Height=info_ptr->height;
+                      // Ideally, we should reserve this color in the palette,
+                      // (so it's not merged and averaged with a neighbor one)
+                      // and after creating the optimized palette, find its
+                      // index and mark it transparent.
+                      
+                      // Current implementation: ignore.
+                    }
+                  }
+                  
+                  context->Width=png_get_image_width(png_ptr,info_ptr);
+                  context->Height=png_get_image_height(png_ptr,info_ptr);
                   
                   png_set_interlace_handling(png_ptr);
                   png_read_update_info(png_ptr, info_ptr);
@@ -3454,7 +3473,7 @@ void Load_PNG(T_IO_Context * context)
                       // 8bpp
                       
                       for (y=0; y<context->Height; y++)
-                        Row_pointers[y] = (png_byte*) malloc(info_ptr->rowbytes);
+                        Row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
                       row_pointers_allocated = 1;
                       
                       png_read_image(png_ptr, Row_pointers);
@@ -3473,7 +3492,7 @@ void Load_PNG(T_IO_Context * context)
                           // It's a preview
                           // Unfortunately we need to allocate loads of memory
                           for (y=0; y<context->Height; y++)
-                            Row_pointers[y] = (png_byte*) malloc(info_ptr->rowbytes);
+                            Row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
                           row_pointers_allocated = 1;
                           
                           png_read_image(png_ptr, Row_pointers);
@@ -3572,9 +3591,15 @@ void Save_PNG(T_IO_Context * context)
           {
             // Commentaires texte PNG
             // Cette partie est optionnelle
+            #ifdef PNG_iTXt_SUPPORTED
+              png_text text_ptr[2] = {
+                {-1, "Software", "Grafx2", 6, 0, NULL, NULL},
+                {-1, "Title", NULL, 0, 0, NULL, NULL}
+            #else
             png_text text_ptr[2] = {
               {-1, "Software", "Grafx2", 6},
               {-1, "Title", NULL, 0}
+            #endif
             };
             int nb_text_chunks=1;
             if (context->Comment[0])

src/help.c
@@ -2,6 +2,7 @@
 */
 /*  Grafx2 - The Ultimate 256-color bitmap paint program
 
+    Copyright 2011 Pawel Góralski
     Copyright 2008 Peter Gordon
     Copyright 2008 Yves Rizoud
     Copyright 2008 Franck Charlet
@@ -31,6 +32,10 @@
     #include <sys/mount.h>
 #elif defined (__linux__)
     #include <sys/vfs.h>
+#elif defined (__MINT__)
+    #include <mint/sysbind.h>
+    #include <mint/osbind.h>
+    #include <mint/ostruct.h>
 #endif
 
 #include "const.h"
@@ -627,7 +632,51 @@ void Button_Stats(void)
   Print_in_window(10,35,"Build options:",STATS_TITLE_COLOR,MC_Black);
   Print_in_window(146,35,TrueType_is_supported()?"TTF fonts":"no TTF fonts",STATS_DATA_COLOR,MC_Black);
 
+#if defined (__MINT__)
+  // Affichage de la mémoire restante
+  Print_in_window(10,43,"Free memory: ",STATS_TITLE_COLOR,MC_Black);
 
+  freeRam=0;
+  char helpBuf[64];
+  
+  unsigned long STRAM,TTRAM;
+  Atari_Memory_free(&STRAM,&TTRAM);
+  freeRam=STRAM+TTRAM;
+  buffer[0]='\0';
+  
+  if(STRAM > (100*1024*1024))
+        sprintf(helpBuf,"ST:%u Mb ",(unsigned int)(STRAM/(1024*1024)));
+  else if(freeRam > 100*1024)
+        sprintf(helpBuf,"ST:%u Kb ",(unsigned int)(STRAM/1024));
+  else
+        sprintf(helpBuf,"ST:%u b ",(unsigned int)STRAM);
+
+  strncat(buffer,helpBuf,sizeof(char)*37);
+  
+  if(TTRAM > (100ULL*1024*1024*1024))
+        sprintf(helpBuf,"TT:%u Gb",(unsigned int)(TTRAM/(1024*1024*1024)));
+  else if(TTRAM > (100*1024*1024))
+        sprintf(helpBuf,"TT:%u Mb",(unsigned int)(TTRAM/(1024*1024)));
+  else if(freeRam > 100*1024)
+        sprintf(helpBuf,"TT:%u Kb",(unsigned int)(TTRAM/1024));
+  else
+        sprintf(helpBuf,"TT:%u b",(unsigned int)TTRAM);
+
+  strncat(buffer,helpBuf,sizeof(char)*37);
+
+  if(freeRam > (100ULL*1024*1024*1024))
+        sprintf(helpBuf,"(%u Gb)",(unsigned int)(freeRam/(1024*1024*1024)));
+  else if(freeRam > (100*1024*1024))
+        sprintf(helpBuf,"(%u Mb)",(unsigned int)(freeRam/(1024*1024)));
+  else if(freeRam > 100*1024)
+        sprintf(helpBuf,"(%u Kb)",(unsigned int)(freeRam/1024));
+  else
+        sprintf(helpBuf,"(%u b)",(unsigned int)freeRam);
+    strncat(buffer,helpBuf,sizeof(char)*37);
+ 
+   Print_in_window(18,51,buffer,STATS_DATA_COLOR,MC_Black);
+
+#else
   // Affichage de la mémoire restante
   Print_in_window(10,51,"Free memory: ",STATS_TITLE_COLOR,MC_Black);
 
@@ -641,8 +690,12 @@ void Button_Stats(void)
         sprintf(buffer,"%u Kilobytes",(unsigned int)(freeRam/1024));
   else
         sprintf(buffer,"%u bytes",(unsigned int)freeRam);
+  
   Print_in_window(114,51,buffer,STATS_DATA_COLOR,MC_Black);
 
+  #endif
+  
+  
   // Used memory
   Print_in_window(10,59,"Used memory pages: ",STATS_TITLE_COLOR,MC_Black);
   if(Stats_pages_memory > (100LL*1024*1024*1024))
@@ -670,6 +723,14 @@ void Button_Stats(void)
       statfs(Main_current_directory,&disk_info);
       mem_size=(qword) disk_info.f_bfree * (qword) disk_info.f_bsize;
     }
+#elif defined (__MINT__)
+   _DISKINFO drvInfo;
+   mem_size=0;
+   Dfree(&drvInfo,0);
+   //number of free clusters*sectors per cluster*bytes per sector;
+   // reports current drive
+   mem_size=drvInfo.b_free*drvInfo.b_clsiz*drvInfo.b_secsiz;
+   
 #else
     // Free disk space is only for shows. Other platforms can display 0.
     #warning "Missing code for your platform !!! Check and correct please :)"

src/helpfile.h
@@ -62,7 +62,11 @@ static const T_Help_table helptable_about[] =
   HELP_BOLD ("         \"Dragon's Layers\" Edition")
   HELP_BOLD ("  THE ULTIMATE MULTI-RESOLUTION GFX EDITOR")
   HELP_TEXT ("       http://grafx2.googlecode.com")
+#if defined(__MINT__)
+  HELP_TEXT ("                atari build                ")
+#else
   HELP_TEXT ("")
+#endif
   HELP_TEXT ("Copyright 2007-2010, the Grafx2 project team")
   HELP_TEXT ("    Copyright 1996-2001, SUNSET DESIGN")
 };
@@ -412,6 +416,10 @@ static const T_Help_table helptable_credits[] =
   HELP_BOLD ("                 SKYOS PORT")
   HELP_TEXT ("")
   HELP_TEXT ("  Luc Schrijvers (Begasus)")
+  HELP_TEXT ("")
+  HELP_BOLD ("                 ATARI PORT")
+  HELP_TEXT ("")
+  HELP_TEXT ("  Pawel Goralski (Saulot)")
   HELP_TEXT ("")
   HELP_TEXT ("")
   HELP_TEXT ("  ... made it work on your favourite toaster")

commit 3a40db66c61e50cdbe94cc1cd3621be408d61987

(release branch) Fix issue: Lose file's current directory, regression introduced in r1709

src/filesel.c
@@ -1236,12 +1236,13 @@ byte Button_Load_or_Save(byte load, T_IO_Context *context)
   else
   {
     #if defined(__MINT__)    
-    chdir(context->File_directory);
     static char path[1024]={0};
+    chdir(context->File_directory);
     Dgetpath(path,0);
     strcat(path,PATH_SEPARATOR);
     strcpy(Main_current_directory,path);  
  #else
+    chdir(context->File_directory);
     getcwd(Main_current_directory,256);
   #endif
 